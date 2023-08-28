25a0a3a0db2209372436163a615c98dd4cb8a789
kushtarbek <dr.kushtarbek@gmail.com>
2023-08-27 22:42:47 -0500

    added inser input, validate, and printboard functions

--- src/Attempt2.java
+++ src/Attempt2.java
@@ -2,29 +2,57 @@ import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Attempt2 {
     @edu.umd.cs.findbugs.annotations.SuppressFBWarnings("DMI_RANDOM_USED_ONLY_ONCE")
     public static void main(String[] args) {
         int [][] board = generateRandomBoard();
+        printBoard(board);
 
-        //Printing the board
-        for (int row = 0; row < 9; row++) {
-            if(row % 3 == 0 && row != 0){
-                System.out.println("---------------------");
+        Scanner scanner = new Scanner(System.in);
+
+        //input numbers and validate
+        while(true){
+            System.out.println("Enter the row (0-8) and column (0-8) of the 3x3 grid, separated by spaces (or -1 to exit):");
+            int row = scanner.nextInt();
+            if(row == -1){
+                break;
             }
-            for (int col = 0; col < 9; col++) {
-                if(col % 3 == 0 && col != 0){
-                    System.out.print("| ");
-                }
-                System.out.print(board[row][col] + " ");
+
+            int col = scanner.nextInt();
+
+            if(row < 0 || row > 6 || col < 0 || col > 6){
+                System.out.println("Invalid input. Please try again.");
+                continue;
             }
-            System.out.println();
+
+            System.out.println("Enter the number (1-9) to insert in the selected 3x3 grid:");
+            int num = scanner.nextInt();
+
+            if(num < 1 || num > 9){
+                System.out.println("Invalid input. Please try again.");
+                continue;
+            }
+
+            int subRow = row % 3;
+            int subCol = col % 3;
+
+            if(!isUniqueInSubgrid(board, row - subRow, col - subCol, num) ||
+                !isUniqueInRowAndColumn(board, row, col, num) ||
+                !isUniqueInSubgridRowAndColumn(board, row - subRow, col - subCol, num)) {
+                System.out.println(" Conflict in subgrid, row, column, or subgrid's row/column. Please try again.");
+                continue;
+            }
+
+            board[row][col] = num;
+            printBoard(board);
         }
 
-
-
+        System.out.println("Exiting the program.");
     }
+
+
     //Generate a random 9x9 board
     @SuppressFBWarnings("DMI_RANDOM_USED_ONLY_ONCE")
     public static int [][] generateRandomBoard(){
@@ -44,6 +72,24 @@ public class Attempt2 {
         }
         return board;
     }
+
+
+    //print the board
+    public static void printBoard( int[][] board ) {
+        for( int row = 0; row < 9; row++ ){
+            if(row % 3 == 0 && row != 0) {
+                System.out.println("-----------------------");
+            }
+            for( int col =0; col < 9; col++){
+                if(col % 3 == 0 && col !=0) {
+                    System.out.print("| ");
+                }
+                System.out.print(board[row][col]+ " ");
+            }
+            System.out.println();
+        }
+    }
+
 
     //Generate a unique number for a given cell, ensuring it is not present in the same row and column
     @SuppressFBWarnings("DMI_RANDOM_USED_ONLY_ONCE")
